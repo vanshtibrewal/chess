d61a9271954d8974ab487c1b0852f964b23c061f
Vansh-T <vanshtibrewal2004@gmail.com>
2023-09-14 12:44:29 -0700

    all movement except king implemented
    base game remaining features:
    king movement
    player-based alternating movement
    check (forced movement)
    checkmate
    stalemate
    history tracking and/or en passant
    
    bonus features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Draggable from 'react-draggable';
 
-function Square({ onClick, onRelease, parity, piece }) {
+function Square({ parity, piece, highlighted }) {
+  let base = (parity ? "light" : "dark");
+  let color = highlighted ? base + "-highlight" : base;
   return (
-    <div className={"square " + (parity ? "light" : "dark")}>
+    <div className={"square " + color}>
       <Draggable position={{x: 0, y: 0}}>
           {getImgFromPiece(piece)}
       </Draggable>
@@ -11,14 +13,14 @@ function Square({ onClick, onRelease, parity, piece }) {
   );
 }
 
-function Board({ board, onClick, onRelease }) {
+function Board({ board }) {
   let b = [];
   let row;
   let p = new Piece();
   for (let i = 0; i < 8; i++) {
     row = [];
     for (let j = 0; j < 8; j++) {
-      row.push(<Square onClick={onClick} onRelease={onRelease} parity={(i + j) % 2 == 0} piece={board[i][j]} key={j}/>);
+      row.push(<Square parity={(i + j) % 2 == 0} piece={board[i][j].piece} highlighted={board[i][j].highlighted} key={j}/>);
     }
     b.push(<div className="board-row" key={i}>{row}</div>);
   }
@@ -27,6 +29,7 @@ function Board({ board, onClick, onRelease }) {
 
 export default function Game() {
   const [board, setBoard] = useState(getBoardFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"));
+  // const [board, setBoard] = useState(getBoardFromFEN("rnbqkbnr/8/8/8/8/8/8/RNBQKBNR"));
   const [localMousePos, setLocalMousePos] = useState({});
   const [clicked, setClicked] = useState({});
 
@@ -41,25 +44,44 @@ export default function Game() {
     let click = {y: Math.floor(localMousePos.y/100), x: Math.floor(localMousePos.x/100)};
     click.y = click.y < 0 ? 0 : click.y;
     click.x = click.x < 0 ? 0 : click.x;
+    click.y = click.y > 7 ? 7 : click.y;
+    click.x = click.x > 7 ? 7 : click.x;
     setClicked(click);
+    if (board[click.y][click.x].piece != null) {
+      let p = board[click.y][click.x].piece;
+      let highlight = p.getLegalMoveSet(board, {x: click.x, y: click.y});
+      for (let i = 0; i < highlight.length; i++) {
+        board[highlight[i].y][highlight[i].x].highlighted = true;
+      }
+    }
   }
 
   function onRelease() {
-    let newBoard = board.slice();
     let click = {y: Math.floor(localMousePos.y/100), x: Math.floor(localMousePos.x/100)};
     click.y = click.y < 0 ? 0 : click.y;
+    click.y = click.y > 7 ? 7 : click.y;
     click.x = click.x < 0 ? 0 : click.x;
-    newBoard[click.y][click.x] = newBoard[clicked.y][clicked.x];
-    newBoard[clicked.y][clicked.x] = null;
-    setBoard(newBoard);
+    click.x = click.x > 7 ? 7 : click.x;
+
+    if (board[click.y][click.x].highlighted) {
+      let newBoard = board.slice();
+      newBoard[click.y][click.x].piece = newBoard[clicked.y][clicked.x].piece;
+      newBoard[clicked.y][clicked.x].piece = null;
+      setBoard(newBoard);
+    }
+
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        board[i][j].highlighted = false;
+      }
+    }
   }
 
   return (
     <div className="game">
-      <div onMouseDown={onClick} onMouseUp={onRelease}className="game-board" onMouseMove={handleMouseMove}>
-        <Board board={board} onClick={onClick} onRelease={onRelease}/>
+      <div onMouseDown={onClick} onMouseUp={onRelease} className="game-board" onMouseMove={handleMouseMove}>
+        <Board board={board}/>
       </div>
-      {Math.floor(localMousePos.y/100) + " " + Math.floor(localMousePos.x/100)}
     </div>
   );
 }
@@ -69,6 +91,85 @@ class Piece {
     this.type = type;
     this.color = color;
     this.movement = movement;
+  }
+
+  isSliding() {
+    return this.type == "b" || this.type == "q" || this.type == "r";
+  }
+
+  getLegalMoveSet(board, position) {
+    if (this.isSliding()) {
+      let legalmoves = [];
+      for (let i = 0; i < this.movement.length; i++) {
+        let dy = this.movement[i][0];
+        let dx = this.movement[i][1];
+        let curry = position.y + dy;
+        let currx = position.x + dx;
+        while (curry >= 0 && curry < 8 && currx >= 0 && currx < 8 && board[curry][currx].piece == null) {
+          legalmoves.push({x: currx, y: curry});
+          curry += dy;
+          currx += dx;
+        }
+        if (curry >= 0 && curry < 8 && currx >= 0 && currx < 8 && board[curry][currx].piece.color != this.color) {
+          legalmoves.push({x: currx, y: curry});
+        }
+      }
+      return legalmoves;
+    }
+    else {
+      if (this.type == "n") {
+        let legalmoves = [];
+        for (let i = 0; i < this.movement.length; i++) {
+          let tary = position.y + this.movement[i][0];
+          let tarx = position.x + this.movement[i][1];
+          if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && (board[tary][tarx].piece == null || board[tary][tarx].piece.color != this.color)) {
+            legalmoves.push({x: tarx, y: tary});
+          }
+        }
+        return legalmoves;
+      }
+      else if (this.type == "p") {
+        let legalmoves = this.getLegalAttackSet(board, position);
+        let dy = this.movement[0][0];
+        let dx = this.movement[0][1];
+        let tary = position.y + dy;
+        let tarx = position.x + dx;
+        if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && board[tary][tarx].piece == null) {
+          legalmoves.push({x: tarx, y: tary});
+        }
+        if ((this.color == "w" && position.y == 6) || (this.color == "b" && position.y == 1)) {
+          tary += dy;
+          tarx += dx;
+          if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && board[tary][tarx].piece == null) {
+            legalmoves.push({x: tarx, y: tary});
+          }
+        }
+        return legalmoves;
+      }
+      else {
+        return []
+      }
+    }
+  }
+
+  getLegalAttackSet(board, position) {
+    if (this.isSliding() || this.type == "n") {
+      return this.getLegalMoveSet(board, position);
+    }
+    else if (this.type == "p") {
+      let legalmoves = [];
+      for (let i = 1; i < this.movement.length; i++){
+        let tary = position.y + this.movement[i][0];
+        let tarx = position.x + this.movement[i][1];
+        if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && board[tary][tarx].piece != null && board[tary][tarx].piece.color != this.color) {
+          legalmoves.push({x: tarx, y: tary});
+        }
+      }
+      return legalmoves;
+    }
+    else {
+      return []
+    }
   }
 }
 
@@ -95,7 +196,7 @@ function makePiece(id, color) {
 }
 
 function getBoardFromFEN(FEN) {
-  let board = Array(8).fill(null).map(() => Array(8).fill(null));
+  let board = Array(8).fill(null).map(() => Array(8).fill(null).map(() => ({piece: null, highlighted: false})));
   let counter = 0;
   for (let i = 0; i < 8; i++) {
     let j = 0;
@@ -106,11 +207,11 @@ function getBoardFromFEN(FEN) {
       }
       else {
         if (char == char.toLowerCase()) {
-          board[i][j] = makePiece(char, "b");
+          board[i][j] = {piece: makePiece(char, "b"), highlighted: false};
         }
         else {
           char = char.toLowerCase();
-          board[i][j] = makePiece(char, "w");
+          board[i][j] = {piece: makePiece(char, "w"), highlighted: false};
         }
         j++;
       }
@@ -129,8 +230,4 @@ function getImgFromPiece(piece) {
     let n = piece.color == "w" ? 0 : 1;
     return (<img position={{x: 0, y: 0}} draggable="false" className="square img" src={"/images/" + piece.type + "_" + n + ".png"}/>);
   }
-}
-
-function mousePosToBoardPos(x, y) {
-  return (x/100, y/100);
 }
