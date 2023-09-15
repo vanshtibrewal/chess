5b103098f6e212230e01754e2462c69827da2602
Vansh-T <vanshtibrewal2004@gmail.com>
2023-09-15 03:12:39 -0700

    massive refactor with class Board

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ function Square({ parity, piece, highlighted }) {
   );
 }
 
-function Board({ board }) {
+function GameBoard({ board }) {
   let b = [];
   let row;
   let p = new Piece();
   for (let i = 0; i < 8; i++) {
     row = [];
-    for (let j = 0; j < 8; j++) {
-      row.push(<Square parity={(i + j) % 2 == 0} piece={board[i][j].piece} highlighted={board[i][j].highlighted} key={j}/>);
+    for (let j = 0; j < 8; j++) { 
+      row.push(<Square parity={(i + j) % 2 == 0} piece={board.grid[i][j]} highlighted={board.highlighter.some(position => (position.y == i && position.x == j))} key={j}/>);
     }
     b.push(<div className="board-row" key={i}>{row}</div>);
   }
@@ -28,8 +28,7 @@ function Board({ board }) {
 }
 
 export default function Game() {
-  const [board, setBoard] = useState(getBoardFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"));
-  // const [board, setBoard] = useState(getBoardFromFEN("rnbqkbnr/8/8/8/8/8/8/RNBQKBNR"));
+  const [board, setBoard] = useState(getBoardFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", false));
   const [localMousePos, setLocalMousePos] = useState({});
   const [clicked, setClicked] = useState({});
 
@@ -47,14 +46,8 @@ export default function Game() {
     click.y = click.y > 7 ? 7 : click.y;
     click.x = click.x > 7 ? 7 : click.x;
     setClicked(click);
-    if (board[click.y][click.x].piece != null) {
-      let p = board[click.y][click.x].piece;
-      let highlight = p.getLegalMoveSet(board, {x: click.x, y: click.y});
-      for (let i = 0; i < highlight.length; i++) {
-        board[highlight[i].y][highlight[i].x].highlighted = true;
-      }
+    setBoard((prevboard) => prevboard.highlight(prevboard.getLegalMoveSet(click)));
     }
-  }
 
   function onRelease() {
     let click = {y: Math.floor(localMousePos.y/100), x: Math.floor(localMousePos.x/100)};
@@ -62,28 +55,188 @@ export default function Game() {
     click.y = click.y > 7 ? 7 : click.y;
     click.x = click.x < 0 ? 0 : click.x;
     click.x = click.x > 7 ? 7 : click.x;
-
-    if (board[click.y][click.x].highlighted) {
-      let newBoard = board.slice();
-      newBoard[click.y][click.x].piece = newBoard[clicked.y][clicked.x].piece;
-      newBoard[clicked.y][clicked.x].piece = null;
-      setBoard(newBoard);
+    if (board.highlighter.some(position => (position.y == click.y && position.x == click.x))) {
+      setBoard((prevboard) => prevboard.makeMove(clicked, click));
     }
-
-    for (let i = 0; i < 8; i++) {
-      for (let j = 0; j < 8; j++) {
-        board[i][j].highlighted = false;
-      }
-    }
+    setBoard((prevboard) => prevboard.resetHighlight());
   }
 
   return (
     <div className="game">
       <div onMouseDown={onClick} onMouseUp={onRelease} className="game-board" onMouseMove={handleMouseMove}>
-        <Board board={board}/>
+        <GameBoard board={board}/>
       </div>
     </div>
   );
+}
+
+class Board {
+  constructor(grid, whitePieces, blackPieces, isBlackTurn, highlighter) {
+    this.grid = grid;
+    this.highlighter = highlighter;
+    this.whitePieces = whitePieces;
+    this.blackPieces = blackPieces;
+    this.isBlackTurn = isBlackTurn;
+  }
+
+  highlight(validMoves) {
+    let newboard = this.clone()
+    for (let i = 0; i < validMoves.length; i++) {
+      newboard.highlighter.push(validMoves[i]);
+    }
+    return newboard;
+  }
+
+  resetHighlight() {
+    let newboard = this.clone();
+    newboard.highlighter = [];
+    return newboard;
+  }
+
+  makeMove(oldPos, newPos) {
+    let newboard = this.clone()
+    if (newboard.isBlackTurn) {
+      newboard.blackPieces = newboard.blackPieces.filter((piece)=>{return !(piece.y == oldPos.y && piece.x == oldPos.x)})
+      newboard.blackPieces.push(newPos);
+      newboard.whitePieces = newboard.whitePieces.filter((piece)=>{return !(piece.y == newPos.y && piece.x == newPos.x)});
+    }
+    else {
+      newboard.whitePieces = newboard.whitePieces.filter((piece)=>{return !(piece.y == oldPos.y && piece.x == oldPos.x)})
+      newboard.whitePieces.push(newPos);
+      newboard.blackPieces = newboard.blackPieces.filter((piece)=>{return !(piece.y == newPos.y && piece.x == newPos.x)});
+    }
+    newboard.isBlackTurn = !newboard.isBlackTurn;
+    newboard.grid[newPos.y][newPos.x] = newboard.grid[oldPos.y][oldPos.x];
+    newboard.grid[oldPos.y][oldPos.x] = null;
+    return newboard;
+  }
+  
+  getLegalMoveSet(click) {
+    let ourpieces = this.isBlackTurn ? this.blackPieces : this.whitePieces;
+    if (!ourpieces.some(position => (position.y == click.y && position.x == click.x))) {
+      return []
+    }
+    let piece = this.grid[click.y][click.x];
+    if (piece.isSliding()) {
+      return this.getSlidingMoveSet(click);
+    }
+    else {
+      if (piece.type == "n") {
+        return this.getKnightMoveSet(click);
+      }
+      else if (piece.type == "p") {
+        return this.getPawnMoveSet(click);
+      }
+      else {
+        return []
+      }
+    }
+  }
+
+  getSlidingMoveSet(click) {
+    let legalmoves = [];
+    let piece = this.grid[click.y][click.x];
+      for (let i = 0; i < piece.movement.length; i++) {
+        let dy = piece.movement[i][0];
+        let dx = piece.movement[i][1];
+        let curry = click.y + dy;
+        let currx = click.x + dx;
+        while (curry >= 0 && curry < 8 && currx >= 0 && currx < 8 && this.grid[curry][currx] == null) {
+          legalmoves.push({x: currx, y: curry});
+          curry += dy;
+          currx += dx;
+        }
+        if (curry >= 0 && curry < 8 && currx >= 0 && currx < 8 && this.grid[curry][currx].color != piece.color) {
+          legalmoves.push({x: currx, y: curry});
+        }
+      }
+      return legalmoves;
+  }
+
+  getKnightMoveSet(click) {
+    let legalmoves = [];
+    let piece = this.grid[click.y][click.x];
+    for (let i = 0; i < piece.movement.length; i++) {
+      let tary = click.y + piece.movement[i][0];
+      let tarx = click.x + piece.movement[i][1];
+      if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && (this.grid[tary][tarx] == null || this.grid[tary][tarx].color != piece.color)) {
+        legalmoves.push({x: tarx, y: tary});
+      }
+    }
+    return legalmoves;
+  }
+  getPawnMoveSet(click) {
+    let legalmoves = [];
+    let piece = this.grid[click.y][click.x];
+    for (let i = 1; i < piece.movement.length; i++){
+      let tary = click.y + piece.movement[i][0];
+      let tarx = click.x + piece.movement[i][1];
+      if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && this.grid[tary][tarx] != null && this.grid[tary][tarx].color != piece.color) {
+        legalmoves.push({x: tarx, y: tary});
+      }
+    }
+
+    let dy = piece.movement[0][0];
+    let dx = piece.movement[0][1];
+    let tary = click.y + dy;
+    let tarx = click.x + dx;
+
+    if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && this.grid[tary][tarx] == null) {
+      legalmoves.push({x: tarx, y: tary});
+    }
+
+    if ((piece.color == "w" && click.y == 6) || (piece.color == "b" && click.y == 1)) {
+      tary += dy;
+      tarx += dx;
+      if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && this.grid[tary][tarx] == null) {
+        legalmoves.push({x: tarx, y: tary});
+      }
+    }
+    return legalmoves;
+  }
+
+  clone() {
+    const newBoard = new Board(
+        this.grid.map(row => row.slice()),
+        this.whitePieces.map(piece => ({...piece})),
+        this.blackPieces.map(piece => ({...piece})),
+        this.isBlackTurn,
+        [...this.highlighter]
+    );
+    return newBoard;
+  }
+}
+
+function getBoardFromFEN(FEN, isBlackTurn) {
+  let grid = Array(8).fill(null).map(() => Array(8).fill(null));
+  let highlighter = [];
+  let whitePieces = [];
+  let blackPieces = [];
+  let counter = 0;
+  for (let i = 0; i < 8; i++) {
+    let j = 0;
+    while (j < 8) {
+      let char = FEN.charAt(counter);
+      if (!isNaN(char)) {
+        j += Number(char);
+      }
+      else {
+        if (char == char.toLowerCase()) {
+          grid[i][j] = makePiece(char, "b");
+          blackPieces.push(generatePosition(i, j));
+        }
+        else {
+          char = char.toLowerCase();
+          grid[i][j] = makePiece(char, "w");
+          whitePieces.push(generatePosition(i, j));
+        }
+        j++;
+      }
+      counter++;
+    }
+    counter++;
+  }
+  return new Board(grid, whitePieces, blackPieces, isBlackTurn, highlighter);
 }
 
 class Piece {
@@ -95,84 +248,6 @@ class Piece {
 
   isSliding() {
     return this.type == "b" || this.type == "q" || this.type == "r";
-  }
-
-  getLegalMoveSet(board, position) {
-    if (this.isSliding()) {
-      return this.getSlidingMoveSet(board, position);
-    }
-    else {
-      if (this.type == "n") {
-        return this.getKnightMoveSet(board, position);
-      }
-      else if (this.type == "p") {
-        return this.getPawnMoveSet(board, position);
-      }
-      else {
-        return []
-      }
-    }
-  }
-
-  getSlidingMoveSet(board, position) {
-    let legalmoves = [];
-      for (let i = 0; i < this.movement.length; i++) {
-        let dy = this.movement[i][0];
-        let dx = this.movement[i][1];
-        let curry = position.y + dy;
-        let currx = position.x + dx;
-        while (curry >= 0 && curry < 8 && currx >= 0 && currx < 8 && board[curry][currx].piece == null) {
-          legalmoves.push({x: currx, y: curry});
-          curry += dy;
-          currx += dx;
-        }
-        if (curry >= 0 && curry < 8 && currx >= 0 && currx < 8 && board[curry][currx].piece.color != this.color) {
-          legalmoves.push({x: currx, y: curry});
-        }
-      }
-      return legalmoves;
-  }
-
-  getKnightMoveSet(board, position) {
-    let legalmoves = [];
-    for (let i = 0; i < this.movement.length; i++) {
-      let tary = position.y + this.movement[i][0];
-      let tarx = position.x + this.movement[i][1];
-      if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && (board[tary][tarx].piece == null || board[tary][tarx].piece.color != this.color)) {
-        legalmoves.push({x: tarx, y: tary});
-      }
-    }
-    return legalmoves;
-  }
-
-  getPawnMoveSet(board, position) {
-    let legalmoves = [];
-    
-    for (let i = 1; i < this.movement.length; i++){
-      let tary = position.y + this.movement[i][0];
-      let tarx = position.x + this.movement[i][1];
-      if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && board[tary][tarx].piece != null && board[tary][tarx].piece.color != this.color) {
-        legalmoves.push({x: tarx, y: tary});
-      }
-    }
-
-    let dy = this.movement[0][0];
-    let dx = this.movement[0][1];
-    let tary = position.y + dy;
-    let tarx = position.x + dx;
-
-    if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && board[tary][tarx].piece == null) {
-      legalmoves.push({x: tarx, y: tary});
-    }
-
-    if ((this.color == "w" && position.y == 6) || (this.color == "b" && position.y == 1)) {
-      tary += dy;
-      tarx += dx;
-      if (tary >= 0 && tary < 8 && tarx >= 0 && tarx < 8 && board[tary][tarx].piece == null) {
-        legalmoves.push({x: tarx, y: tary});
-      }
-    }
-    return legalmoves;
   }
 }
 
@@ -198,31 +273,8 @@ function makePiece(id, color) {
   }
 }
 
-function getBoardFromFEN(FEN) {
-  let board = Array(8).fill(null).map(() => Array(8).fill(null).map(() => ({piece: null, highlighted: false})));
-  let counter = 0;
-  for (let i = 0; i < 8; i++) {
-    let j = 0;
-    while (j < 8) {
-      let char = FEN.charAt(counter);
-      if (!isNaN(char)) {
-        j += Number(char);
-      }
-      else {
-        if (char == char.toLowerCase()) {
-          board[i][j] = {piece: makePiece(char, "b"), highlighted: false};
-        }
-        else {
-          char = char.toLowerCase();
-          board[i][j] = {piece: makePiece(char, "w"), highlighted: false};
-        }
-        j++;
-      }
-      counter++;
-    }
-    counter++;
-  }
-  return board;
+function generatePosition(y, x) {
+  return {y: y, x: x};
 }
 
 function getImgFromPiece(piece) {
